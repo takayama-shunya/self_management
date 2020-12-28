5e10a802181f6050b129de4e4fa14d3e99d1374e
takayama shunya <katonbo1@icloud.com>
2020-12-28 19:09:54 +0900

    conditionグラフ表示

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem 'devise-i18n'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'chart-js-rails', '~> 0.1.4'
+gem 'bootstrap'
+gem 'jquery-rails'
+gem 'gon', '~> 6.2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,51 @@
 class TopController < ApplicationController
+  
+  before_action :authenticate_user!
+  before_action :what_day_condition, only: %i[ index ]
+  before_action :condition_value, only: %i[ index ]
+  before_action :condition_average_value, only: %i[ index ]
+
+
   def index
-    @date = Time.now
   end
+
+  private
+
+  def set_top_condition
+    @condition = current_user.conditions.find_by(created_at: @date.in_time_zone.all_day)
+  end
+
+  def what_day_condition
+    if @date = params[:date_condition]
+      set_top_condition
+    else
+      @date = Time.now
+      set_top_condition
+    end
+  end
+
+  def condition_value
+    unless @condition.blank?
+      gon.value = [
+        @condition.sleep_time, @condition.sleep_quality,
+        @condition.meal_count, @condition.stress_level, 
+        @condition.toughness, @condition.stress_recovery_balance, 
+        @condition.positive_level, @condition.enrichment_happiness_level
+      ]
+    end
+  end
+
+  def condition_average_value
+    condition = Condition.where(user_id: current_user.id)
+    gon.average_value = [
+      condition.average(:sleep_time).to_i, condition.average(:sleep_quality).to_i,
+      condition.average(:meal_count).to_i, condition.average(:stress_level).to_i,
+      condition.average(:toughness).to_i, condition.average(:stress_recovery_balance).to_i,
+      condition.average(:positive_level).to_i, condition.average(:enrichment_happiness_level).to_i
+    ]
+  end
+
 end
+
+# @tasks = Task.includes(user: :labels).where(user_id: current_user.id)
+
