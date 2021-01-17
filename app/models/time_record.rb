9575814f7bd200a99731742ae8b5f09b5e066883
takayama shunya <katonbo1@icloud.com>
2021-01-17 13:11:05 +0900

    weeks_day テーブル ポリモーフィックに変更

--- app/models/time_record.rb
+++ app/models/time_record.rb
@@ -1,7 +1,7 @@
 class TimeRecord < SettingRecord
 
   belongs_to :user
-  has_many :week_days, dependent: :destroy
+  has_many :week_days, as: :recordable, dependent: :destroy
   has_many :weeks, through: :week_days
   has_one :comment, as: :commentable, dependent: :destroy
 
--- db/migrate/20210101142519_create_week_days.rb
+++ db/migrate/20210101142519_create_week_days.rb
@@ -1,11 +1,7 @@
 class CreateWeekDays < ActiveRecord::Migration[5.2]
   def change
     create_table :week_days do |t|
-      t.integer :integer_record_id
-      t.integer :decimal_record_id
-      t.integer :time_record_id
-      t.integer :check_record_id
-      t.integer :count_record_id
+      t.references :recordable, polymorphic: true, index: true
       t.integer :week_id
       t.index :week_id
       t.timestamps
