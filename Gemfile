640152f61ea612c0ff47d6c5d88505a6531fa6c6
takayama shunya <katonbo1@icloud.com>
2021-01-25 18:34:25 +0900

    seedデータ作成

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap'
 gem 'jquery-rails'
 gem 'gon', '~> 6.2.0'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,70 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+week_day =[0,1,2,3,4,5,6]
+week_day.each do |day|
+  Week.create!(
+    dayname: day
+  )
+end
+
+3.times do |n|
+  User.create!(
+    name: "user_#{n + 1}",
+    email: "user_#{n + 1}@icloud.com",
+    password: "password" )
+end
+
+condition_list_1 = []
+condition_list_2 = []
+user_1 = User.find_by(name: "user_1")
+user_2 = User.find_by(name: "user_2")
+15.times do |n|
+  condition_list_1 << 
+    { retirung_time: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :midnight),
+      rising_time: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :morning),
+      sleep_time: rand(1..5),
+      sleep_quality: rand(1..5),
+      meal_count: rand(1..5),
+      stress_level: rand(1..5),
+      toughness: rand(1..5),
+      stress_recovery_balance: rand(1..5),
+      positive_level: rand(1..5),
+      enrichment_happiness_level: rand(1..5),
+      user_id: user_1.id,
+      created_at: (n - 1).days.ago }
+end
+5.times do |n|
+  condition_list_2 << 
+    { retirung_time: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :midnight),
+      rising_time: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :morning),
+      sleep_time: rand(1..5),
+      sleep_quality: rand(1..5),
+      meal_count: rand(1..5),
+      stress_level: rand(1..5),
+      toughness: rand(1..5),
+      stress_recovery_balance: rand(1..5),
+      positive_level: rand(1..5),
+      enrichment_happiness_level: rand(1..5),
+      user_id: user_2.id,
+      created_at: (n - 1).days.ago }
+end
+Condition.create!(condition_list_1)
+Condition.create!(condition_list_2)
+
+model = [ IntegerRecord, DecimalRecord, TimeRecord, CountRecord ]
+model.each do |m|
+  m.create!(
+    title: "#{m}",
+    unit: "test",
+    content: rand(1..99),
+    user_id: user_1.id,
+    week_ids: Week.ids.sample
+  )
+end
+CheckRecord.create!(
+  title: "CheckRecord",
+  unit: "",
+  content: false,
+  user_id: user_1.id,
+  week_ids: Week.ids.sample
+)
+
+
