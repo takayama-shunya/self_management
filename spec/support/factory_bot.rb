e69de0105e7ef15e9915c41abe53b207e3b8d1ce
takayama shunya <katonbo1@icloud.com>
2021-02-08 20:51:37 +0900

    記録一覧画面文言追加 integer_record3桁区切りに変更

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,10 +1,10 @@
 module FactoryBot
-  def self.find_or_create(name, *traits, **attributes, &block)
+  def self.find_or_create(name, traits, attributes, &block)
     klass = FactoryBot.build(name).class
-    attrs = FactoryBot.attributes_for(name, *traits).merge(attributes)
+    attrs = FactoryBot.attributes_for(name, traits).merge(attributes)
     find_attrs = klass.new(attrs).attributes
     if klass.ancestors.include?(ActiveRecord::Base)
-      find_attrs.slice!(*klass.connection.indexes(klass.table_name).select(&:unique).flat_map(&:columns).uniq)
+      find_attrs.slice!(klass.connection.indexes(klass.table_name).select(&:unique).flat_map(&:columns).uniq)
     end
     klass.find_by(find_attrs, &block) || FactoryBot.create(name, attrs, &block)
   end
