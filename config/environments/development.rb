e993cea4a4043f806b15a711f51c150a36ff6453
takayama shunya <katonbo1@icloud.com>
2021-02-15 01:14:34 +0900

    メール設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    enable_starttls_auto: true,
+    port:587,
+    user_name: ENV['USER_NAME'],
+    password: ENV['PASSWORD'],
+    authentication: :login
+  }
 end
