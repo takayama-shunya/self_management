1dc7a413483b7a15ed41b516cb0a7e9caf6289d3
takayama shunya <katonbo1@icloud.com>
2021-02-15 06:01:42 +0900

    action_mailer.smtp_settings 編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,7 @@ Rails.application.configure do
     port:587,
     user_name: ENV['USER_NAME'],
     password: ENV['PASSWORD'],
-    authentication: :login
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end
