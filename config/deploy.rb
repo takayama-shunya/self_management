c8bb5901615287e53f7f0ba985ae02bac9a7193c
takayama shunya <katonbo1@icloud.com>
2021-02-15 03:02:20 +0900

    config アプリ名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:takayama-shunya/selef_management.git'
 
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/selef_management'
+set :deploy_to, '/var/www/self_management'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
