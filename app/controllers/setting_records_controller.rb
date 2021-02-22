231240721982ade59a404e9ff2fd15a1d6db6e55
takayama shunya <katonbo1@icloud.com>
2021-02-23 03:35:22 +0900

    record関連検索記述 modelに移動

--- app/controllers/setting_records_controller.rb
+++ app/controllers/setting_records_controller.rb
@@ -3,7 +3,7 @@ class SettingRecordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @records = SettingRecord.where(user_id: current_user.id).page(params[:page])
+    @records = SettingRecord.find_by_user(current_user.id).page(params[:page])
   end
  
 end
--- app/models/setting_record.rb
+++ app/models/setting_record.rb
@@ -1,4 +1,7 @@
 class SettingRecord < ApplicationRecord
+  
 
+  scope :find_by_user, ->(user_id) { where(user_id: user_id) }
+  scope :implementation_date, ->(dayname) { joins(:weeks).where(weeks: {dayname: dayname}) }
 
 end
