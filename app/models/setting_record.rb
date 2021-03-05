2aa76fe4d1be91b171b031a2b940f8643d5fdab6
takayama shunya <katonbo1@icloud.com>
2021-03-06 03:17:09 +0900

    record累計値機能 コールバック修正

--- app/models/setting_record.rb
+++ app/models/setting_record.rb
@@ -1,7 +1,7 @@
 class SettingRecord < ApplicationRecord
   
-  # after_save :cumlative_value_create
-  # after_update :cumlative_value_update
+  after_create :cumlative_value_create
+  after_update :cumlative_value_update
 
   scope :find_by_user, ->(user_id) { where(user_id: user_id) }
   scope :implementation_date, ->(dayname) { joins(:weeks).where(weeks: {dayname: dayname}) }
@@ -12,31 +12,32 @@ class SettingRecord < ApplicationRecord
   private
 
   def cumlative_value_create
-    if self.type != "CheckRecord"
+    unless self.type == "CheckRecord"
       cumlative_value = self.build_cumlative_value(value: self.content) 
       cumlative_value.save
     end
   end
 
-  def cumlative_value_uodate
-    if self.type != "CheckRecord"
-      cumlative_value = CumlativeValue.find(self.id)
-      cumlative_value.update(set_record_action(self.type))
+  def cumlative_value_update
+    if self.saved_change_to_attribute?("content")
+      unless self.type == "CheckRecord"
+        cumlative_value = CumlativeValue.find_by(recordable_id: self.id)  
+        cumlative_value.update!(set_record_action(self.type))
+      end
     end
   end
 
   def set_record_action(type)
     case type
     when "IntegerRecord" || "CountRecord"
-      { value: (@cumlative_value.value.to_i + self.content.to_i).to_s }
+      { value: (cumlative_value.value.to_i + self.content.to_i).to_s }
     when "DecimalRecord"
-      { value: (@cumlative_value.value.to_f + self.content.to_f).to_s }
+      { value: (cumlative_value.value.to_f + self.content.to_f).truncate(3).to_s }
     when "TimeRecord"
-      time_record_value = (@cumlative_value.delete(':').to_i + self.copntent.delete(':').to_i).to_s
+      time_record_value = (cumlative_value.value.delete(':').to_i + self.content.delete(':').to_i).to_s
       time_record_value = time_record_value.insert(time_record_value.length - 2, ":")
       { value: time_record_value }
     end
-
   end
 
 
